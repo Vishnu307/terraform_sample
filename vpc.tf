08b8175ccc916a9b3f59c487dcd7ac091ae2d2d4
Vishnu307 <vishnuvardan.chowki@pearson.com>
2022-10-22 02:38:01 +0530

    first commit

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 #Creation of VPC
 resource "aws_vpc" "my_vpc" { 
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = var.vpccidr
   instance_tenancy = "default"
 
   tags = {
@@ -8,22 +8,3 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
-#Creation of 2 Public subnets with count function
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
-  count = 2
-  tags = {
-    Name = "public_subnet ${count.index}"
-  }
-}
-
-#Creation of 2 Private subnets with count function
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.2.0/24"
-  count = 2
-  tags = {
-    Name = "private_subnet ${count.index}"
-  }
-}
